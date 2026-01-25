71e8368b354a4dfcc2fd39dfca836aff838b84a8
grae ceney <gceney7@gmail.com>
2026-01-25 19:42:34 +0000

    added a time counter for logging

---
+++ funcs.py
@@ -0,0 +1,11 @@
+import time
+
+
+def timeCounter(func):
+    def wrap(*args, **kwargs):
+        start = time.time()
+        result = func(*args, **kwargs)
+        end = time.time()
+        total = (end - start).__floor__()
+        return result, total
+    return wrap
